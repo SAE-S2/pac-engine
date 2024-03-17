e12516a309e72e747f6ab101cfb1ffee6fcff696
Achille Decaux <achille.decaux@etud.u-picardie.fr>
2024-03-17 14:06:57 +0100

    Create base

---
+++ pac-engine/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using pac_engine.Graphics;
+
+namespace pac_engine
+{
+	public class Game
+	{
+		private string title;
+		private Vector2 screenSize;
+
+		public Game(string title, float x, float y)
+		{
+			this.title = title;
+			this.screenSize = new Vector2(x, y);
+		}
+	}
+}
---
+++ pac-engine/Graphics/Vector2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pac_engine.Graphics
+{
+	public class Vector2
+	{
+		public float x, y;
+
+		public Vector2() { this.x = 0.0f; this.y = 0.0f; }
+        public Vector2(float xy) { this.x = xy; this.y = xy; }
+        public Vector2(float x, float y) { this.x = x; this.y = y; }
+
+		public bool Equals(Vector2 vector)
+		{
+			return (this.x == vector.x && this.y == vector.y);
+		}
+
+		public float Distance(Vector2 vector)
+        {
+            float diffX = this.x - vector.x;
+            float diffY = this.y - vector.y;
+
+            return (float)Math.Sqrt(diffX * diffX + diffY * diffY);
+        }
+    }
+}
