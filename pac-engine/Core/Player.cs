95bea9c934e7b1a9a1d047d10ee0238981d70c85
Théo Tornatore <tornatoretheo@gmail.com>
2024-03-25 14:16:15 +0100

    Add(Movement): Create async loop

--- pac-engine/Core/Player.cs
+++ pac-engine/Core/Player.cs
@@ -1,18 +1,55 @@
-﻿namespace pac_engine.Core
+﻿using pac_engine.Utils;
+namespace pac_engine.Core
 {
 	public class Player : Entity
- 	{
-        public int money;
-        public int bolts;
+	{
+		public int money;
+		public int bolts;
+		public int angle = 0;
 
-        public Player()
+		public Player()
 		{
-	        this.maxHealth = 3.5f;
-	        this.Health = 2.5f;
-	        this.speed = 1.0f;
-	        this.damage = 0.0f;
-	        this.money = 100;
-	        this.bolts = 2;
+			this.maxHealth = 3.5f;
+			this.Health = 2.5f;
+			this.speed = 1.0f;
+			this.damage = 0.0f;
+			this.money = 100;
+			this.bolts = 2;
+		}
+
+		public async Task Movement()
+		{
+			await Task.Run(() =>
+			{
+				while (this.Health > 0)
+				{
+					switch (this.angle)
+					{
+						case 0:
+							this.pos.y += 1;
+							break;
+						case 1:
+							this.pos.x += 1;
+							break;
+						case 2:
+							this.pos.y -= 1;
+							break;
+						case 3:
+							this.pos.x -= 1;
+							break;
+					}
+                    Console.WriteLine($"Position: x={this.pos.x}, y={this.pos.y}");
+                    Task.Delay(Globals.ENTITY_SPEED * (int)this.speed).Wait();
+				}
+			});
+		}
+		public void AngleChange(int angle)
+		{
+			if (this.angle < 0 && this.angle > 3)
+			{
+				return;
+			}
+			this.angle = angle;
 		}
 	}
 }
