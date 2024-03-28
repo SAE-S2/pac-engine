c18ed479e220c37484684be1e8fefe0f9970320e
Théo Tornatore <tornatoretheo@gmail.com>
2024-03-28 13:58:54 +0100

    Edit(Vector): Surcharge des opérateurs +,-,==,!=

--- pac-engine/Utils/Vector2.cs
+++ pac-engine/Utils/Vector2.cs
@@ -8,11 +8,6 @@
         public Vector2(int xy) { this.x = xy; this.y = xy; }
         public Vector2(int x, int y) { this.x = x; this.y = y; }
 
-		public bool Equals(Vector2 vector)
-		{
-			return (this.x == vector.x && this.y == vector.y);
-		}
-
 		public float Distance(Vector2 vector)
         {
             int diffX = this.x - vector.x;
@@ -26,10 +21,18 @@
 			return new Vector2((int)(this.x * (1 - percent) + vector.x * percent), (int)(this.y * (1 - percent) + vector.y * percent));
 		}
 
-		public void Add(Vector2 vector)
-		{
-			this.x += vector.x;
-			this.y += vector.y;
-		}
+        private bool Equals(Vector2 vector)
+        {
+            return (this.x == vector.x && this.y == vector.y);
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b) 
+			=> new Vector2(a.x + b.x, a.y + b.y);
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+            => new Vector2(a.x - b.x, a.y - b.y);
+        public static bool operator ==(Vector2 a, Vector2 b)
+            => a.Equals(b);
+        public static bool operator !=(Vector2 a, Vector2 b)
+            => !(a.Equals(b));
     }
 }
