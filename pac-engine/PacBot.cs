6d66dab6abe4e6288e707577647ceb68ae33d111
Achille Decaux <achille.decaux@etud.u-picardie.fr>
2024-05-05 11:53:44 +0200

    Add(Game): base

--- pac-engine/PacBot.cs
+++ pac-engine/PacBot.cs
@@ -1,6 +1,5 @@
 ﻿using pac_engine.Utils;
 using pac_engine.Core;
-using System.Reflection.Emit;
 
 namespace pac_engine
 {
@@ -10,6 +9,9 @@ namespace pac_engine
 		private Vector2 screenSize;
         private Player player;
         public Game ActualGame;
+        public string name;
+        public int price;
+
 
 		public PacBot(string title, int x, int y)
 		{
@@ -17,7 +19,6 @@ namespace pac_engine
 			screenSize = new Vector2(x, y);
             Console.Title = this.title;
             //Console.SetWindowSize(width: (int)this.screenSize.x, height: (int)this.screenSize.y);
-            player = new Player();
         }
 
         public bool StartGame()
@@ -27,20 +28,139 @@ namespace pac_engine
             return ActualGame.Start();
         }
 
+        public string[] GetProfils()
+        {
+            string[] profils = { "Profil 1", "Profil 2" };
+            return profils;
+        }
+
         static void Main()
         {
             PacBot pacbot = new PacBot(title: "test", x: 500, y: 500);
 
-            bool win = pacbot.StartGame();
 
-            if (win)
+            string[] profils = pacbot.GetProfils(); // Récupération des profils
+
+            bool createProfil = false;
+            if (profils.Length < 1)
             {
-                Console.WriteLine("Gagné");
+                createProfil = true;
+            }
+            else if (profils.Length < 3)
+            {
+                Console.WriteLine("Créer un nouveau profil (O/N)");
+                createProfil = Console.ReadKey().Key == ConsoleKey.O;
+            }
+
+            if (createProfil)
+            {
+                // creation profil
+                Console.WriteLine("Entrer pseudo: ");
+                pacbot.name = Console.ReadLine();
+
+                // TODO: Insert en bdd
+
+                // Level de debut
+                pacbot.StartGame();
             }
             else
             {
-                Console.WriteLine("Perdu");
+                // Choix profil
+                int selected = -1;
+                while (selected < 0 || profils.Length < selected)
+                {
+                    Console.WriteLine("Choisir un profil:");
+                    for (int i = 0; i < profils.Length; i++)
+                    {
+                        Console.WriteLine("[" + (i + 1) + "]: " + profils[i]);
+                    };
+
+                    ConsoleKey input = Console.ReadKey().Key;
+                    if (input == ConsoleKey.D1)
+                    {
+                        selected = 0;
+                    }
+                    else if (input == ConsoleKey.D2)
+                    {
+                        selected = 1;
+                    }
+                    else if (input == ConsoleKey.D3)
+                    {
+                        selected = 2;
+                    }
+                }
+                // Récupération des données du joueur
+                pacbot.name = profils[selected];
+                //...
+                // Chargement de toutes les données relative au profil
             }
+
+            // Creation du joueur en fonction des stats
+            pacbot.player = new Player();
+
+            // Chargement du prix actuel du jeu
+            pacbot.price = 0;
+
+            bool lose = false;
+            int level = 1;
+            while (level < 11)
+            {
+                if (level == 1)
+                {
+                    Console.WriteLine("Argent: " + pacbot.player.money);
+                    Console.WriteLine("Prix de sortie: " + pacbot.price);
+                    Thread.Sleep(2000);
+                    if (pacbot.player.money < pacbot.price)
+                    {
+                        lose = true;
+                        level = 11;
+                    }
+                }
+
+                if (!lose)
+                {
+                    bool win = pacbot.StartGame(); // TODO: Ajouter level en param pour la génération du monde
+
+                    if (win)
+                    {
+                        level++;
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Passage au prochain niveau ("+level+")");
+                        pacbot.player.Heal(pacbot.player.maxHealth - pacbot.player.Health);
+                        Thread.Sleep(2000);
+                    }
+                    else
+                    {
+                        level = 1;
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Vous êtes mort");
+                        Console.WriteLine("Vous revenez en prison");
+                        pacbot.player.Heal(pacbot.player.maxHealth - pacbot.player.Health);
+                        if (pacbot.price == 0)
+                        {
+                            pacbot.price += 10;
+                        }
+                        pacbot.price += (int) Math.Floor(pacbot.price * 0.2);
+                        Thread.Sleep(2000);
+                    }
+                }
+            }
+
+            if (lose)
+            {
+                Console.WriteLine("Vous n'avez plus assez d'argent.");
+                Console.WriteLine("Vous avez perdu.");
+                // Supprimer le profil ?
+            }
+            else
+            {
+                Console.WriteLine("Vous avez fini le jeu");
+            }
+
         }
     }
 }
