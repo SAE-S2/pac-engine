bc36b9f2eb839189732544966bc42413421f5f6e
Nassim7500 <154921119+Nassim7500@users.noreply.github.com>
2024-05-19 15:52:01 +0200

    Add event.cs

---
+++ pac-engine/Utils/event.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace pac_engine.Utils
+{
+    public class PositionChangedEventArgs
+    {
+        public Vector2 NewPos { get; set; }
+        public Vector2 OldPos { get; set; }
+    }
+
+    public class EarnCoinEventArgs 
+    {
+        public Vector2 Pos { get; set;}
+    }
+}
