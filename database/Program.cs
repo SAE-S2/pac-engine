1fca7ccbfbad2364f9595e4a095ebce1b1bb055b
Léo <leo.robillard04@gmail.com>
2024-05-24 14:28:05 +0200

    FS4.3(Edit): Nouvelle Version du code

--- database/Program.cs
+++ database/Program.cs
@@ -1,35 +1,128 @@
-﻿using MySqlConnector;
-namespace database
-{
-    public class connect
-    {
-        public static MySqlDataReader Main(string query)
-        {
-            var builder = new MySqlConnectionStringBuilder
-            {
-                Server = "10.1.139.236",
-                UserID = "a3 ",
-                Password = "PacBot2024",
-                Database = "basea3",
-            };
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using MongoDB.EntityFrameworkCore.Extensions;
 
-            using var cn = new MySqlConnection(builder.ToString());
-            cn.Open();
-            using var cmd = new MySqlCommand(query, cn);
-            return cmd.ExecuteReader();
-        }
-        
-        public string[] getProfile(int UID)
+
+
+public class Evasion
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    public int NumEvasion { get; set; }
+    public int UtilisationPouvoirs { get; set; }
+    public int HPPerdus { get; set; }
+    public int EnnemisTues { get; set; }
+    public int NbPiece { get; set; }
+    public int NbBoulon { get; set; }
+    public int Score { get; set; }
+    public int NumAmelioration { get; set; }
+    public int IDProfil { get; set; }
+}
+public class Equipement_Possede
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    public int IDProfil { get; set; }       
+    public int NumAmelioration { get; set; }
+    public int NiveauAmelioration { get; set; }
+}
+internal class Utilisateur
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    public int UID { get; set; }
+    public string login { get; set; }
+    public string MdpUtilisateur { get; set; }
+}
+public class Profil
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    public int IDProfil { get; set; }
+    public int NumProfil { get; set; }
+    public string NomProfil { get; set; }
+    public bool Level10Played { get; set; }
+    public bool Dialogue_Garde{ get; set; }
+    public bool Dialogue_Prison { get; set; }
+    public bool Dialogue_Debut { get; set; }
+    public bool Dialogue_Inge { get; set; }
+    public int TotalPieces { get; set; }
+    public int TotalBoulons { get; set; }
+    public int UID { get; set; }
+}
+public class Amelioration
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+
+    public int NumAmelioration { get; set; }
+    public int Rarete { get; set; }
+    public string NomAmelioration { get; set; }
+    public string Descripption { get; set; }
+    public bool Est_Equipable { get; set; }
+}
+
+public class PacBotContext : DbContext
+{
+    public DbSet<Utilisateur> Utilisateurs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Remplacez par votre chaîne de connexion MongoDB
+        var connectionString = "mongodb+srv://PacBot:PacBot2024@pacbot.yzzchig.mongodb.net/?retryWrites=true&w=majority&appName=PacBot";
+        var databaseName = "PacBot";
+
+        optionsBuilder.UseMongoDB(connectionString, databaseName);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuration supplémentaire si nécessaire
+    }
+}
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var context = new PacBotContext();
+
+        // Ajouter un nouvel utilisateur
+        var nouvelUtilisateur = new Utilisateur
         {
-            string[] profile = [];
-            int i=0;
-            MySqlDataReader reader = Main($"SELECT * FROM profile WHERE UID ={UID}");
-            while (reader.Read())
-            {
-                profile[i] = reader["UID"].ToString + ";" + reader["NomProfil"].ToString;
-                i++;
-            }
-            return profile;
+            UID = 3,
+            login = "PacTester",
+            MdpUtilisateur = "123456789"
+        };
+
+        await context.Utilisateurs.AddAsync(nouvelUtilisateur);
+        await context.SaveChangesAsync();
+        Console.WriteLine("Utilisateur ajouté avec succès");
+
+        // Récupérer tous les utilisateurs
+        var utilisateurs = await context.Utilisateurs.ToListAsync();
+        foreach (var utilisateur in utilisateurs)
+        {
+            Console.WriteLine($"UID: {utilisateur.UID}, Login: {utilisateur.login}");
         }
     }
 }
+
+
+
