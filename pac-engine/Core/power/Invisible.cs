d447121419e2dabf876d1255a5dbc6495232e2e2
Nassim7500 <nassim.idris@etud.u-picardie.fr>
2024-05-30 09:57:22 +0200

    Add(power): Add powers

---
+++ pac-engine/Core/power/Invisible.cs
@@ -0,0 +1,47 @@
+﻿
+namespace pac_engine.Core
+{
+    public class Damage : Power
+    {
+        public Damage(int levelToSet)
+        {
+            level = levelToSet;
+        }
+
+        public async Task Active(Player player)
+        {
+            if (!CanActive())
+                return;
+
+            cooldown = true;
+            player.imortal = true;
+            player.damage = 10f;
+
+            await Task.Run(() =>
+            {
+                switch (level)
+                {
+                    case 1:
+                        Thread.Sleep(10000);
+                        break;
+                    case 2:
+                        Thread.Sleep(12000);
+                        break;
+                    case 3:
+                        Thread.Sleep(15000);
+                        break;
+                }
+
+                player.imortal = false;
+                player.damage = 0f;
+
+                if (level == 3)
+                    Thread.Sleep(180000);
+                else
+                    Thread.Sleep(240000);
+
+                cooldown = false;
+            });
+        }
+    }
+}
---
+++ pac-engine/Core/power/Shield.cs
@@ -0,0 +1,43 @@
+﻿
+using pac_engine.Core.power;
+using System.Reflection.Emit;
+
+namespace pac_engine.Core
+{
+    public class Shield : Power
+    {
+        public Shield(int levelToSet)
+        {
+            level = levelToSet;
+        }
+
+        public async Task Active(Player player)
+        {
+            if (!CanActive())
+                return;
+
+            cooldown = true;
+            if (level == 3)
+                player.absorption = 1.5f;
+            else
+                player.absorption = 1.0f;
+
+            await Task.Run(() =>
+            {
+                if (level == 1)
+                    Thread.Sleep(30000);
+                else
+                    Thread.Sleep(45000);
+
+                player.absorption = 0.0f;
+
+                if (level == 3)
+                    Thread.Sleep(180000);
+                else
+                    Thread.Sleep(240000);
+
+                cooldown = false;
+            });
+        }
+    }
+}
